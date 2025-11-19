61d4c74e0c411e29696fdee3247183b9c3cbe310
Saim Anis <saimdesign87@gmail.com>
2025-11-19 20:51:17 +0500

    Mobile Sidebar State management

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,12 @@ const [menu,setMenu] = useState(false)
         } className="mt-3 bg-blue-500 p-2 rounded text-white" />
         <h1 className="italic text-2xl font-bold mt-3">Lifeline</h1>
       </div></NavLink>
-      <ul className="flex gap-9 text-1xl font-medium sm:flex hidden">
+
+
+
+
+
+      <ul className="flex-row gap-9 text-1xl font-medium lg:flex  hidden" >
 
         <NavLink to="/">
           <li  py-1>Home</li>
@@ -75,10 +80,57 @@ const [menu,setMenu] = useState(false)
     </div>:
      <button onClick={()=>navigate('./login')} className="bg-blue-500 p-3 rounded-3xl  text-[#fff] hover:bg-blue-700">Create Account</button>
   }
-  <span>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <span onClick={() => setMenu(true)}>
  <FontAwesomeIcon icon={faBars 
-        } className="mt-3 block sm:hidden" />
+        } className="mt-3 block sm:flex  lg:hidden" />
         </span>
+ 
+        {menu && (
+          
+          <div className="fixed top-0 bottom-0 left-0 w-[100vw] h-[100vh] bg-white shadow shadow-black/100 bg-opacity-100 z-50 flex flex-col text-center">
+         <ul className=" flex flex-col  gap-9  font-medium mt-36 text-3xl " >
+ <span onClick={() => setMenu(false)}>
+ <FontAwesomeIcon icon={faBars 
+        }  />
+        </span>
+        <NavLink to="/">
+          <li  py-1>Home</li>
+        
+        </NavLink>
+        <NavLink to="/doctors">
+          <li py-1>Doctors</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/appointment">
+          <li py-1>Appointment</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/about">
+          <li py-1>About</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        
+        <NavLink to="/contact">
+          <li py-1>Contact</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden"  />
+        </NavLink>
+
+      </ul>
+      </div>
+      )}
 </div>
 
      
